18973d17a8b9b6d5d9fb6c1598d4f82e90a2bde9
Alexandr <alexandr.bosenko@gmail.com>
2023-09-16 17:46:29 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person("Alex", "Bosenko");
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        String json = gson.toJson(person);
+        System.out.println(json);
+    }
+}
